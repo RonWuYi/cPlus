0035afc15fcc398f0890ba6fc8d7eacf2bd9d3c5
RonWuYi <lyshmily@outlook.com>
2017-11-14 11:43:10 -0800

    modified:   CMakeLists.txt 	new file:   stone.cpp 	new file:   stone.h 	modified:   stonewt.cpp 	new file:   stonewt.h 	deleted:    stonewt.h.h

---
+++ stone.h
@@ -0,0 +1,8 @@
+//
+// Created by Ron on 11/14/2017.
+//
+
+#ifndef CPLUS_STONE_H
+#define CPLUS_STONE_H
+
+#endif //CPLUS_STONE_H
--- stonewt.h.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Ron on 11/14/2017.
-//
-
-#ifndef CPLUS_STONEWT_H_H
-#define CPLUS_STONEWT_H_H
-
-#endif //CPLUS_STONEWT_H_H
