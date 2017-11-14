8bb8346db29e059f5c501e3635c2fdf86857e74b
RonWuYi <lyshmily@outlook.com>
2017-11-14 12:41:38 -0800

    modified:   CMakeLists.txt 	modified:   stringbad.cpp 	modified:   strngbad.h 	modified:   vegnews.cpp

--- strngbad.h
+++ strngbad.h
@@ -1,8 +1,20 @@
 //
 // Created by Ron on 11/14/2017.
 //
-
-#ifndef CPLUS_STRNGBAD_H
-#define CPLUS_STRNGBAD_H
-
+#include <iostream>
+#ifndef STRNGBAD_H_
+#define STRNGBAD_H_
+class StringBad
+{
+private:
+    char * str;
+    int len;
+    static int num_strings;
+public:
+    StringBad(const char * s);
+    StringBad();
+    ~StringBad();
+    // friend function
+    friend std::ostream &operator<<(std::ostream & os, const StringBad & st);
+};
 #endif //CPLUS_STRNGBAD_H
--- vegnews.cpp
+++ vegnews.cpp
@@ -1,4 +1,44 @@
 //
 // Created by Ron on 11/14/2017.
 //
+#include <iostream>
+using std::cout;
+#include "strngbad.h"
 
+void callme1(StringBad &);
+void callme2(StringBad);
+
+int main(){
+    using std::endl;
+    StringBad headline1("Celery Stalks at Midnight");
+    StringBad headline2("Lettuce Prey");
+    StringBad sports("Spinach Leaves Bowl for Dollars");
+    cout << "headline1: " << headline1 << endl;
+    cout << "headline2: " << headline2 << endl;
+    cout << "sports: " << sports << endl;
+    callme1(headline1);
+    cout << "headline1: " << headline1 << endl;
+    callme2(headline2);
+    cout << "headline1: " << headline1 << endl;
+    cout <<"Initialize one object to another: \n";
+    StringBad sailor = sports;
+    cout << "sailor: " << sailor << endl;
+    cout << "assign one object to another: \n";
+    StringBad knot;
+    knot = headline1;
+    cout << "knot: " << knot << endl;
+    cout << "end of main()\n";
+
+    std::cin.get();
+    return 0;
+}
+
+void callme1(StringBad & rsb){
+    cout << "String passed by reference: \n";
+    cout << "\"" <<rsb<<"\"\n";
+}
+
+void callme2(StringBad rsb){
+    cout << "String passed by value: \n";
+    cout << "\"" <<rsb<<"\"\n";
+}
