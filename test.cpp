999d9e97588699757ff0d2a0612e58c60edae73f
RonWuYi <lyshmily@outlook.com>
2017-11-14 07:14:52 -0800

    new file:   111.cpp 	modified:   CMakeLists.txt 	new file:   randwalk.cpp 	new file:   test.cpp 	new file:   vect.cpp 	modified:   vect.h

---
+++ test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by HFD on 11/14/2017.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    cout << sin(30) << endl;
+    cin.get();
+    return 0;
+}
