f3e2e17f0fff0e3d41e1a169b96408f3f23b84fd
RonWuYi <lyshmily@outlook.com>
2017-11-21 07:00:37 -0800

    modified:   bank.cpp 	new file:   tabtenn0.cpp 	new file:   tabtenn0.h 	modified:   test.cpp 	new file:   usett0.cpp

--- test.cpp
+++ test.cpp
@@ -8,22 +8,12 @@
 #include <cstdlib>
 
 using namespace std;
-bool newcustomer(double x){
-    return (std::rand()*x/RAND_MAX < 1);
-}
+//bool newcustomer(double x){
+//    return (std::rand()*x/RAND_MAX < 1);
+//}
 int main(){
-    int total = 0;
-    for (double i = 1.0; i < 61.0; i++) {
-        cout << newcustomer(i) << "  during i = "<< i <<" rand()= " <<rand()<<endl;
-
-        if(newcustomer(i)==1)
-            total++;
-    }
-
-    cout << "total = "<<total << endl;
-
-    cout << "RAND_MAX = "<<RAND_MAX << endl;
-    cout << "srand(std::time(0)) = "<< srand(std::time(0)) << endl;
+    std::srand(std::time(0));
+    cout << rand() <<endl;
     cin.get();
     return 0;
 }
