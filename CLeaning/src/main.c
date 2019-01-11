ebe71b9d5e5d6a2bcb3cf13121b97bf965258aaa
Ron <RonWuYi@users.noreply.github.com>
2019-01-11 06:29:37 -0800

    Update main.c

--- CLeaning/src/main.c
+++ CLeaning/src/main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 
-int main()
+#define MAXLINE 1000
+
+int get_line(char line[], int maxline);
+
+int main(void)
 {
-    char buffer[50];
-    int n, a=5, b=3;
-    n=sprintf(buffer, "%d plus %d is %d", a, b, a+b);
-    printf("[%s] is a string %d chars long\n", buffer, n);
+    int len; /* current line length */
+    char line[MAXLINE]; /* current input line */
+
+    while ((len = get_line(line, MAXLINE)) > 0) {
+        if (len == 1 && line[0] == '\n')
+            continue;
+        printf("%s\n", line);
+    }
+
     return 0;
+}
+
+int get_line(char s[], int lim)
+{
+    int c, i, l;
+
+    for (i = 0, l = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+        if (i < lim - 1)
+            s[l++] = c;
+    if (c == '\n' && l < lim - 1)
+        s[l++] = c;
+    s[l] = '\0';
+
+    return l;
 }
