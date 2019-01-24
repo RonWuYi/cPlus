432437693d7981784933f68153f07adaa3823e9c
Ron <RonWuYi@users.noreply.github.com>
2019-01-24 05:39:31 -0800

    Update a.h

--- CLeaning/src/a.h
+++ CLeaning/src/a.h
@@ -5,9 +5,116 @@
 #ifndef CPLUS_A_H
 #define CPLUS_A_H
 
-typedef unsigned int        uc_uint32;
-typedef unsigned short      uc_uint16;
-typedef short               uc_int16;
-typedef unsigned char       uc_byte;
-typedef char *              uc_string;
+typedef unsigned int    uc_uint32;  /**< Unsigned 32-bit integer */
+typedef unsigned short  uc_uint16;  /**< Unsigned 16-bit integer */
+typedef short           uc_int16;  /**< Signed 16-bit integer */
+typedef unsigned char   uc_byte;    /**< Unsigned 8-bit byte */
+typedef char *          uc_string;  /**< Null-terminated string */
+typedef char            uc_char;    /**< Single character */
+typedef int             uc_sint32;  /**< Signed 32-bit integer */
+typedef unsigned char   uc_uint8;   /**< Unsigned 8-bit integer */
+typedef signed char     uc_int8;    /**< Signed 8-bit integer */
+
+typedef int             uc_intptr;  /**< Signed int of the same size as a pointer */
+typedef unsigned int    uc_uintptr; /**< Unsigned int of the same size as a pointer */
+
+typedef struct _HostDeviceDataPersonalizedData
+{
+    uc_uint32 vLength;
+    uc_uint8 vData[UC_PRE_ENABLEMENT_PERSONALIZED_DATA_MAXSIZE];
+}HostDeviceDataPersonalizedData;
+
+typedef struct _HostDeviceDataPrivateData
+{
+    uc_uint32 vLength;
+    uc_uint8 vData[UC_PRIVATE_DATA_MAXSIZE];
+}HostDeviceDataPrivateData;
+
+typedef struct _HostDeviceData
+{
+    uc_uint8 vCssn[UC_CSSN_SIZE];
+    uc_uint8 vCsuk[UC_CSUK_SIZE];
+    uc_uint8 vCpuk[UC_CPUK_SIZE];
+#if defined(INCL_TRANSFORMATION)
+    uc_uint8 vTauk[UC_TAUK_SIZE];
+#endif
+    uc_bool  vIs3LevelKeyLadder;
+#ifdef INCL_PULLEMM
+    uc_uint8 vPINCode[UC_PIN_CODE_SIZE];
+    uc_uint8 vMAC[UC_MAC_SIZE];
+#endif
+    HostDeviceDataPrivateData vPrivateData;  /* modify */
+    HostDeviceDataPersonalizedData vPersonalizedData;
+    uc_uint8 vOTPIndexCnt;
+#ifdef INCL_PULLEMM
+    uc_bool  vValidPrivateData;
+    uc_bool  vValidCSSN;
+#endif
+}HostDeviceData;
+
+extern "C" uc_result UT_GetDeviceData(HostDeviceData *vHostDeviceData)
+{
+    uc_result vResult = UC_ERROR_SUCCESS;
+    FILE *vFile = NULL;
+
+    memset((MW_UCHAR *)vHostDeviceData, 0x0, sizeof(HostDeviceData));
+
+    if(UC_ERROR_SUCCESS == vResult)
+    {
+        CString vDeviceFileName;
+        LPCTSTR vDeviceFileNameStr = NULL;
+
+        vDeviceFileName.Format("%s%s", UT_GetExePath(), UT_DEVICE_FILE_NAME);
+        vDeviceFileNameStr = vDeviceFileName;
+        
+        /*Try to open device file.*/
+        if( 0 != fopen_s( &vFile, (const char *)vDeviceFileNameStr, "rb" )) 
+        {
+            /*Failed to open a file.*/
+            vResult = UC_ERROR_MISSING_DATA;
+            vFile = NULL;
+        }
+    }
+    if(UC_ERROR_SUCCESS == vResult)
+    {
+        size_t vBytesRead = fread( vHostDeviceData, 1, sizeof(HostDeviceData), vFile );
+        if(sizeof(HostDeviceData) != vBytesRead)
+        {
+            vResult = UC_ERROR_MISSING_DATA;
+        }
+    }
+
+    if(vFile)
+        fclose(vFile);
+
+    return vResult;
+}
+
+void UT_SetExePath()
+{
+    TCHAR vPath[MAX_PATH];
+    memset(vPath, 0x0, sizeof(vPath));
+    GetModuleFileName( NULL, vPath, MAX_PATH );
+    int i = 0, j = 0;
+    while( vPath[i]!=0 )
+    {
+        if( vPath[i]=='\\' )
+            j = i;
+        i++;
+    }
+    vPath[j+1] = '\0';
+    gUtExePath.Format( "%s", vPath );
+}
+
+/*Get current exe path.*/
+CString UT_GetExePath()
+{
+    if(gUtExePath.IsEmpty())
+    {
+        UT_SetExePath();
+        UT_SetDeviceDefault();
+    }
+    return gUtExePath;
+}
+
 #endif //CPLUS_A_H
